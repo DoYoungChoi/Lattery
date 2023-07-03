bd844bc10ec63be981ef7984cedc3caede7733b4
dodor <choidy9298@naver.com>
2023-07-03 11:35:01 +0900

    Initial Commit

---
+++ Lattery/LatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatteryApp.swift
+//  Lattery
+//
+//  Created by dodor on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct LatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
