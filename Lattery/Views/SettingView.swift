9ee53de539fd48f5f5b52be761c55dacb13e5c5a
dodor <choidy9298@naver.com>
2023-07-31 01:03:10 +0900

    FEAT: 저장 로또 번호 보기 기본 구현

--- Lattery/Views/SettingView.swift
+++ Lattery/Views/SettingView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text("설정")
+        List {
+            Section {
+                NavigationLink("저장된 로또 결과", destination: SavedLotto())
+                NavigationLink("즐겨찾는 로또 번호 조합", destination: SavedLotto())
+            }
+        }
+        .navigationTitle("설정")
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+struct SavedLotto: View {
+    @EnvironmentObject var data: LottoData
+    @FetchRequest(sortDescriptors: []) var lottos: FetchedResults<LottoResult>
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(lottos) { lotto in
+                    ScrollView(.horizontal) {
+                        HStack {
+                            Text("\(lotto.date ?? Date())")
+                            Text("\(lotto.lotteryDate ?? "No Lottery Date")")
+                            Text("\(lotto.aGroup ?? "aGroup")")
+                            Text("\(lotto.bGroup ?? "bGroup")")
+                            Text("\(lotto.cGroup ?? "cGroup")")
+                            Text("\(lotto.dGroup ?? "dGroup")")
+                            Text("\(lotto.eGroup ?? "eGroup")")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
